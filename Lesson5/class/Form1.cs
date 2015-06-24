b7ef3e7758a9e922bf1317441d2253b25e93fde8
unknown <blue13@RM02-13.fasfg.local>
2015-06-24 15:20:25 +0100

    new lesson and project

---
+++ Lesson5/class/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vehicle myCar = new Vehicle();
+            myCar.Make = "DeLorean";
+            myCar.Model = "Time Machine";
+            myCar.ElapsedMilage = 9999969;
+            myCar.MarketPrice = 1000000;
+
+            //MessageBox.Show(myCar.MarketPrice.ToString());
+
+            string result;
+            result = myCar.Drive(30);
+            MessageBox.Show(result);
+        }
+    }
+}
---
+++ Lesson5/class/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Vehicle
+    {
+        private string _make;
+        private string _model;
+        private int _elapsedMilage;
+        private int _marketPrice;
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int ElapsedMilage
+        {
+            get { return _elapsedMilage; }
+            set { _elapsedMilage = value; }
+        }
+
+        public int MarketPrice
+        {
+            get { return _marketPrice; }
+            set { _marketPrice = value; }
+        }
+
+        public string Drive(int kilometers)
+        {
+            _elapsedMilage += kilometers;
+
+
+            string result;
+            result = "The " + Make +" " + Model+ " now has " + _elapsedMilage + " kilometers, and market price is " + 
+                _marketPrice + "€.";
+            return result;
+        }
+
+    }
